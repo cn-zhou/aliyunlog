2b8a364f94bdf69de065d37e6fe4b433d2b13ed8
周阳 <zhouyang@theduapp.com>
2019-12-03 16:39:34 +0800

    调整保存时间为2天

--- file_hook.go
+++ file_hook.go
@@ -13,7 +13,7 @@ func getFileHook(p, n string) *lfshook.LfsHook {
 	baseLogPath := path.Join(p, n)
 	writer, err := rotatelogs.New(
 		baseLogPath+"-%Y-%m-%d-%H.log",
-		rotatelogs.WithMaxAge(3*24*time.Hour),  // 文件最大保存时间
+		rotatelogs.WithMaxAge(1*24*time.Hour),  // 文件最大保存时间
 		rotatelogs.WithRotationTime(time.Hour), // 日志切割时间间隔
 	)
 	if err != nil {
